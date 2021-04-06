3d6e7dfc064a057ccfdfc0cd7901b67b054d7387
Yan Santana <yansbarreiro@gmail.com>
2021-04-06 16:23:47 -0300

    :electric_plug: integrating with stripe api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head';
-import styles from './home.module.scss';
+import {GetServerSideProps} from 'next';
+import { SubscribeButton } from '../components/SubscribeButton';
 
-export default function Home() {
+import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
+
+interface HomeProps{
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
     <Head>
@@ -13,11 +24,31 @@ export default function Home() {
         <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to all the publications <br/>
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
         </p>
+        <SubscribeButton priceId={product.priceId} />
       </section>
       <img src="/images/avatar.svg" alt="girl coding"/>
     </main>
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IdK0mCzX2MlHtWEv3T3vfMq')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  };
+
+  return{
+    props:{
+      product,
+    }
+  }
 }
